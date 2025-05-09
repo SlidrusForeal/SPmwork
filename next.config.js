a3085d19b4a2677ade7422bdc01bdcb3e4201d61
SlidrusForeal <49024907+SlidrusForeal@users.noreply.github.com>
2025-05-09 19:28:08 +0300

    вва

--- next.config.js
+++ next.config.js
@@ -1,11 +1,18 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    env: {
-        NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
-        NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-        NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
-    }
+  reactStrictMode: true,
+  
+  // Разрешаем внешние домены для next/image
+  images: {
+    domains: ['minotar.net', 'crafatar.com'],
+  },
+
+  env: {
+    NEXT_PUBLIC_BASE_URL: process.env.NEXT_PUBLIC_BASE_URL,
+    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
+    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  }
 };
 
 module.exports = nextConfig;
